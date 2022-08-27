b877d1d9d7d2e35513f4dc8d24a44c5d29efcfeb
David Blatt <davidblatt10@gmail.com>
2022-08-27 09:08:43 -0400

    updated @heroicons/react

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,5 +1,5 @@
 import { Disclosure } from '@headlessui/react'
-import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { NavLink } from 'react-router-dom'
 import { IoLogoGameControllerB } from 'react-icons/io'
 
@@ -37,9 +37,9 @@ function NavBar() {
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md hover:text-primary hover:bg-base-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-bg-neutral-content">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                   )}
                 </Disclosure.Button>
               </div>
